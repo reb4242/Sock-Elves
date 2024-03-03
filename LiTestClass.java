d78c6e7d400016a77e1a9f6fe9cd25b97a28b62f
Rebecca Li <148259352+reb4242@users.noreply.github.com>
2024-03-02 22:57:13 -0500

    Update and rename Test to LiTestClass.java

---
+++ LiTestClass.java
@@ -0,0 +1,15 @@
+/**
+ * This is the test class that can be used by the elves and humans. 
+ *
+ * @author (Rebecca)
+ * @version (Nov.20th/23)
+ */
+public class LiTestClass{
+    public static void main(String[] args){
+        //declare map objects
+        MapCreator Map1 = new MapCreator();
+        
+        //read map from files
+        MapReading ReadMap1 = new MapReading("Map#1.txt");
+    }
+}
